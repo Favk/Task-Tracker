985d3d334770d9d4d39f0eca8bf0e6fe45b65e27
Favk <fok0786@mavs.uta.edu>
2022-07-06 17:29:07 +0100

    email form still not working

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import "../../../assets/smtp.js";
-import "../../../../.env";
 import { NgForm } from "@angular/forms";
 import { EmailService } from "../../services/email.service";
 
